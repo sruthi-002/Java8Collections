253990314e114da936f45745bb107be5e0102632
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-05 17:01:00 +0530

    Add files via upload

---
+++ Collectionsjava8.java
@@ -0,0 +1,18 @@
+﻿package com.company;
+import java.util.Spliterator;
+import java.util.Scanner;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       ArrayList<Integer> list = new ArrayList<Integer>();
+       Scanner in = new Scanner(System.in);
+       int n,i;
+       n = in.nextInt();
+       for(i=0;i<n;i++)
+       {
+           list.add(in.nextInt());
+       }
+       Spliterator<Integer> Students = list.spliterator();
+       Students.forEachRemaining(System.out::println);
+    }
+}
